078e3c536305affcac105920d1a17e358ba574a9
lkhod <lkhodory@gmail.com>
2026-03-13 17:32:43 -0500

    added a couple of useful test methods

--- Lab 5/test.py
+++ Lab 5/test.py
@@ -1,19 +1,33 @@
 
+def HashPasswords():
+    with open('credentials.txt', 'r') as credentialsFile:
+        loginFile = credentialsFile.read().split()
+    loginInfo = [['' for x in range(2)] for y in range(50)]
+    for i in range(100):
+        loginInfo[int(i / 2)][i % 2] = loginFile[i]
 
-with open('credentials.txt', 'r') as credentialsFile:
-    loginFile = credentialsFile.read().split()
-loginInfo = [['' for x in range(2)] for y in range(50)]
-for i in range(100):
-    loginInfo[int(i / 2)][i % 2] = loginFile[i]
+    from cryptography.hazmat.primitives import hashes
+    digest = hashes.Hash(hashes.SHA256())
+    with open('hashedCredentials.txt', 'w') as hashedFile:
+        for i in range(50):
+            newDigest = digest.copy()
+            newDigest.update(bytes(loginInfo[i][1], 'utf-8'))
+            hashedFile.write(loginInfo[i][0] + ' ')
+            hashedFile.write(str(newDigest.finalize()) + '\n')
 
-from cryptography.hazmat.primitives import hashes
+def TestHashArray():
+    with open('hashedCredentials.txt', 'r') as credentialsFile:
+        loginFile = credentialsFile.read().split('\n')
 
-digest = hashes.Hash(hashes.SHA256())
-with open('hashedCredentials.txt', 'w') as hashedFile:
+    loginInfo = [['' for x in range(2)] for y in range(50)]
     for i in range(50):
-        newDigest = digest.copy()
-        newDigest.update(bytes(loginInfo[i][1], 'utf-8'))
-        hashedFile.write(loginInfo[i][0] + ' ')
-        hashedFile.write(str(newDigest.finalize()) + '\n')
-        i = i + 2
+        loginInfo[i][0] = loginFile[i].split(' ')[0]
+        loginInfo[i][1] = loginFile[i].split(' ')[1]
+    print(loginInfo)
+
+
+if __name__ == "__main__":
+    # HashPasswords()
+    TestHashArray()
+
 
