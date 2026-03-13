031bc3ee4a4af4c93853cdc35606664416a0937e
lkhod <lkhodory@gmail.com>
2026-03-13 15:23:12 -0500

    test successfully creates hashed passwords

---
+++ Lab 5/test.py
@@ -0,0 +1,19 @@
+
+
+with open('credentials.txt', 'r') as credentialsFile:
+    loginFile = credentialsFile.read().split()
+loginInfo = [['' for x in range(2)] for y in range(50)]
+for i in range(100):
+    loginInfo[int(i / 2)][i % 2] = loginFile[i]
+
+from cryptography.hazmat.primitives import hashes
+
+digest = hashes.Hash(hashes.SHA256())
+with open('hashedCredentials.txt', 'w') as hashedFile:
+    for i in range(50):
+        newDigest = digest.copy()
+        newDigest.update(bytes(loginInfo[i][1], 'utf-8'))
+        hashedFile.write(loginInfo[i][0] + ' ')
+        hashedFile.write(str(newDigest.finalize()) + '\n')
+        i = i + 2
+
