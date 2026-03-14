6e2a52a879b6556466b2c0e90dfeb42d11d2dfc9
lkhod <lkhodory@gmail.com>
2026-03-13 20:12:01 -0500

    fixed a problem with putting hashed passwords into an array

--- Lab 5/Lab5Server.py
+++ Lab 5/Lab5Server.py
@@ -34,20 +34,146 @@ def decryptMessage(key: bytes, data: bytes) -> bytes:
     return unpadder.update(padded) + unpadder.finalize()
 
 
+def checkCreds(username, password):
+    
+    ## Read in hashed credentials from file
+    with open('hashedCredentials.txt', 'r') as credentialsFile:
+        credentials = credentialsFile.read().split()
+
+    loginInfo = [['' for _ in range(2)] for _ in range(50)]
+    for i in range(100): loginInfo[int(i / 2)][i % 2] = credentials[i]
+
+    myDigest = hashes.Hash(hashes.SHA256())
+    myDigest.update(bytes(password, 'utf-8'))
+    hashedPasswordAttempt = str(myDigest.finalize())
+
+    ## Read in hashed credentials from file
+    with open('hashedCredentials.txt', 'r') as credentialsFile:
+        credentials = credentialsFile.read().split('\n')
+
+    ## convert stored credentials into 2D array
+    loginInfo = [['' for _ in range(2)] for _ in range(50)]
+    for i in range(50):
+        loginInfo[i][0] = credentials[i].split(' ', 1)[0]
+        loginInfo[i][1] = credentials[i].split(' ', 1)[1]
+
+    for i in range(50):
+        print(i)
+        print(loginInfo[i][0])
+        print(loginInfo[i][1] + '\n')
+
+    ## Check the credential files to see whether the username
+    ##      exists and password is correct
+    goodPassword = False
+    goodUsername = False
+    usernameIndex = 0
+    for i in range(50):
+        if username == loginInfo[i][0]: goodUsername = True
+        usernameIndex += 1
+    if loginInfo[usernameIndex][1] == hashedPasswordAttempt:
+        goodPassword = True
+
+    print('username: ' + username + '\n')
+    print(hashedPasswordAttempt + '\n')
+    print(loginInfo[usernameIndex][1])
+
+    return goodUsername, goodPassword
+
+
 def main():
+    replyMessage = 'login successful'
+    badUsername = 'user name does not exist\n'
+    badPassword = 'incorrect password\n'
 
     ###################
     # Socket Creation #
     ###################
 
-    # serverPort = 34567
-    ## create a welcome TCP socket
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    # serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # serverSocket.bind(("", serverPort))
-
     serverSocket.bind(('localhost', 8089))
+    serverSocket.listen(1)
+    print("The server is ready to receive requests")
+    connectSocket, addr = serverSocket.accept()
 
+    ##########################
+    # Server Protocol Design #
+    ##########################
+
+    ## Read in hashed credentials from file
+    with open('hashedCredentials.txt', 'r') as credentialsFile:
+        credentials = credentialsFile.read().split('\n')
+
+    ## convert stored credentials into 2D array
+    loginInfo = [['' for x in range(2)] for y in range(50)]
+    for i in range(50):
+        loginInfo[i][0] = credentials[i].split(' ')[0]
+        loginInfo[i][1] = credentials[i].split(' ')[1]
+
+    print(loginInfo)
+
+    goodPassword = False
+    goodUsername = False
+
+    while 1:
+
+        encryptedRequest = connectSocket.recv(1024)
+
+        request = decryptMessage(SECRET_KEY, encryptedRequest).decode("ascii")
+
+        ## Parse the request message to obtain username and password
+        requestCommand = request.split('\t')[0]
+
+        ## Setting up for future request types
+        ## Line is also here to make PyCharm shut up about warnings
+        if requestCommand == "Disconnect": break
+
+        ## Handling of login request information
+        username = ''
+        password = ''
+        if requestCommand == "Login":
+            username = request.split('\t')[1]
+            password = request.split('\t')[2]
+
+        myDigest = hashes.Hash(hashes.SHA256())
+        myDigest.update(bytes(password, 'utf-8'))
+        hashedPasswordAttempt = str(myDigest.finalize())
+
+        ## Check the credential files to see whether the username
+        ##      exists and password is correct
+        usernameIndex = 0
+        for i in range(50):
+            if username == loginInfo[i][0]:
+                goodUsername = True
+                break
+            usernameIndex = usernameIndex + 1
+
+        if loginInfo[usernameIndex][1] == hashedPasswordAttempt:
+            goodPassword = True
+
+        ## construct message with information about incorrect input
+        if not goodPassword: replyMessage = badPassword
+        if not goodUsername: replyMessage = badUsername
+
+        ## Construct the login response message and encrypt it before
+        ##      sending back to the client
+        encryptedReply = encryptMessage(SECRET_KEY, replyMessage.encode())
+        connectSocket.send(encryptedReply)
+        if goodPassword: break
+
+    connectSocket.close()
+
+
+def Test():
+    replyMessage = 'login successful'
+    badUsername = 'user name does not exist\n'
+    badPassword = 'incorrect password\n'
+
+    ###################
+    # Socket Creation #
+    ###################
+
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('localhost', 8089))
     serverSocket.listen(1)
     print("The server is ready to receive requests")
     connectSocket, addr = serverSocket.accept()
@@ -58,123 +184,40 @@ def main():
 
     while 1:
 
-        replyMessage = ''
-        badUsername = 'user name does not exist\n'
-        badPassword = 'incorrect password\n'
-        successMessage = 'successful'
-
         encryptedRequest = connectSocket.recv(1024)
-        # request = connectSocket.recv(1024).decode("ascii")
-        # print('encrypted message: ', request)
-        # print('encrypted message: ', encryptedRequest)
 
         request = decryptMessage(SECRET_KEY, encryptedRequest).decode("ascii")
-        print('request data: ', request)
 
         ## Parse the request message to obtain username and password
         requestCommand = request.split('\t')[0]
 
         ## Setting up for future request types
-        ## Line is also here to make PyCharm shut up about warnings
         if requestCommand == "Disconnect": break
 
         ## Handling of login request information
-        userName = ''
+        username = ''
         password = ''
         if requestCommand == "Login":
-            userName = request.split('\t')[1]
+            username = request.split('\t')[1]
             password = request.split('\t')[2]
-            print("Username is: ", userName)
-            print("Password is: ", password)
-
-        ## Read in hashed credentials from file
-        with open('hashedCredentials.txt', 'r') as credentialsFile:
-            credentials = credentialsFile.read().split()
-
-        loginInfo = [['' for x in range(2)] for y in range(50)]
-        for i in range(100): loginInfo[int(i / 2)][i % 2] = credentials[i]
-
-        myDigest = hashes.Hash(hashes.SHA256())
-        myDigest.update(bytes(password, 'utf-8'))
-        hashedPasswordAttempt = str(myDigest.finalize())
-
-        ## Read in hashed credentials from file
-        with open('hashedCredentials.txt', 'r') as credentialsFile:
-            credentials = credentialsFile.read().split('\n')
-
-        ## convert stored credentials into 2D array
-        loginInfo = [['' for x in range(2)] for y in range(50)]
-        for i in range(50):
-            loginInfo[i][0] = credentials[i].split(' ')[0]
-            loginInfo[i][1] = credentials[i].split(' ')[1]
 
         ## Check the credential files to see whether the username
-        goodUsername = False
-        usernameIndex = 0
-        for i in range(50):
-            if userName == loginInfo[i][0]:
-                print('username found')
-                goodUsername = True
+        ##      exists and password is correct
+        goodUsername, goodPassword = checkCreds(username, password)
 
         ## construct message with information about incorrect input
-        if (goodUsername and loginInfo[usernameIndex][1] ==
-                hashedPasswordAttempt):
-            replyMessage = successMessage
+        if not goodPassword: replyMessage = badPassword
         if not goodUsername: replyMessage = badUsername
-        else: replyMessage = badPassword
 
         ## Construct the login response message and encrypt it before
         ##   sending back to the client
         encryptedReply = encryptMessage(SECRET_KEY, replyMessage.encode())
         connectSocket.send(encryptedReply)
+        if goodPassword: break
 
     connectSocket.close()
 
 
-def Test():
-
-    ##########################
-    # Server Protocol Design #
-    ##########################
-
-    replyMessage = ''
-    badUsername = 'user name does not exist\n'
-    badPassword = 'incorrect password\n'
-    successMessage = 'successful'
-
-    ## Handling of login request
-    userName = "painters"
-    password = "vp2aNk"
-    print("Username is: ", userName)
-    print("Password is: ", password)
-
-    myDigest = hashes.Hash(hashes.SHA256())
-    myDigest.update(bytes(password, 'utf-8'))
-    hashedPasswordAttempt = str(myDigest.finalize())
-
-    ## Read in hashed credentials from file
-    with open('hashedCredentials.txt', 'r') as credentialsFile:
-        credentials = credentialsFile.read().split('\n')
-
-    loginInfo = [['' for x in range(2)] for y in range(50)]
-    for i in range(50):
-        loginInfo[i][0] = credentials[i].split(' ')[0]
-        loginInfo[i][1] = credentials[i].split(' ')[1]
-
-    ## Check the credential files to see whether the username
-    ##   exists and password matches
-    goodUsername = False
-    usernameIndex = 0
-    for i in range(50):
-        if userName == loginInfo[i][0]:
-            print('username found')
-            goodUsername = True
-
-    if not goodUsername: print(badUsername)
-    if goodUsername and loginInfo[usernameIndex][1] != hashedPasswordAttempt:
-        print(successMessage)
-
-
 if __name__ == "__main__":
-    main()
-    # Test()
+    # main()
+    Test()
