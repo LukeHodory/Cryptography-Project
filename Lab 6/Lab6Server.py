500111801652fe27cf9fa099bdbd44a305cb86b7
lkhod <lkhodory@gmail.com>
2026-03-18 19:07:38 -0500

    client and server files added from lab 5

---
+++ Lab 6/Lab6Server.py
@@ -0,0 +1,125 @@
+
+# !/usr/sbin/python3
+from socket import *
+import os
+# import struct
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+SECRET_KEY = b"0123456789abcdef"  # 16 bytes = AES-128 (demo key)
+BLOCK_SIZE_BITS = 128
+
+
+def encryptMessage(key: bytes, plaintext: bytes) -> bytes:
+    iv = os.urandom(16)
+    padder = padding.PKCS7(BLOCK_SIZE_BITS).padder()
+    padded = padder.update(plaintext) + padder.finalize()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv),
+                    backend=default_backend())
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded) + encryptor.finalize()
+    return iv + ciphertext
+
+
+def decryptMessage(key: bytes, data: bytes) -> bytes:
+    iv = data[:16]
+    ciphertext = data[16:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv),
+                    backend=default_backend())
+    decryptor = cipher.decryptor()
+    padded = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(BLOCK_SIZE_BITS).unpadder()
+    return unpadder.update(padded) + unpadder.finalize()
+
+
+def checkCreds(username, password):
+
+    ## Create hash digest
+    myDigest = hashes.Hash(hashes.SHA256())
+    myDigest.update(bytes(password, 'utf-8'))
+    hashedPasswordAttempt = str(myDigest.finalize())
+
+    ## Read in hashed credentials from file
+    with open('HashedCredentials.txt', 'r') as credentialsFile:
+        credentials = credentialsFile.read().split('\n')
+
+    ## Put usernames and passwords into 2d array
+    loginInfo = [['' for _ in range(2)] for _ in range(50)]
+    for i in range(50):
+        loginInfo[i][0] = credentials[i].split(' ', 1)[0]
+        loginInfo[i][1] = credentials[i].split(' ', 1)[1]
+
+    ## Find index of username, if it exists
+    goodUsername = False
+    usernameIndex = 0
+    for i in range(50):
+        if username == loginInfo[i][0]:
+            goodUsername = True
+            break
+        usernameIndex += 1
+
+    ## Check password associated with username
+    goodPassword = False
+    if goodUsername:
+        goodPassword = (loginInfo[usernameIndex][1] == hashedPasswordAttempt)
+
+    return goodUsername, goodPassword
+
+
+def main():
+    replyMessage = 'login successful'
+    badUsername = 'user name does not exist\n'
+    badPassword = 'incorrect password\n'
+
+    ###################
+    # Socket Creation #
+    ###################
+
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('localhost', 8089))
+    serverSocket.listen(1)
+    print("The server is ready to receive requests")
+    connectSocket, addr = serverSocket.accept()
+
+    ##########################
+    # Server Protocol Design #
+    ##########################
+
+    while 1:
+
+        ## Receive request from client
+        encryptedRequest = connectSocket.recv(1024)
+        request = decryptMessage(SECRET_KEY, encryptedRequest).decode("ascii")
+
+        ## Parse the request message to obtain username and password
+        requestCommand = request.split('\t')[0]
+
+        ## Handling of login request information
+        username = ''
+        password = ''
+        if requestCommand == "Disconnect": break
+        if requestCommand == "Login":
+            username = request.split('\t')[1]
+            password = request.split('\t')[2]
+
+        ## Check the credential files to see whether the username
+        ##      exists and password is correct
+        goodUsername, goodPassword = checkCreds(username, password)
+
+        ## Construct message with information about incorrect input
+        if not goodPassword: replyMessage = badPassword
+        if not goodUsername: replyMessage = badUsername
+
+        ## Construct the login response message and encrypt it before
+        ##   sending back to the client
+        encryptedReply = encryptMessage(SECRET_KEY, replyMessage.encode())
+        connectSocket.send(encryptedReply)
+        if goodPassword: break
+
+    connectSocket.close()
+
+
+if __name__ == "__main__":
+    main()
